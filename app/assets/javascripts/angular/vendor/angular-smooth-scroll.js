043ed73ac2ddc90f5c1e614e42d1ac5ab60bd1af
ryan <lrcrews@gmail.com>
2015-11-21 17:03:40 -0800

    breaking stuff

--- app/assets/javascripts/angular/vendor/angular-smooth-scroll.js
+++ app/assets/javascripts/angular/vendor/angular-smooth-scroll.js
@@ -235,8 +235,6 @@
 
           e.preventDefault();
 
-          $("#header-sub-menu").css('display', 'none');
-
           var callbackBefore = function(element) {
             if ( $attrs.callbackBefore ) {
               var exprHandler = $scope.callbackBefore({element: element});
