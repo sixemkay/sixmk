2cf0684720ef54de1bb45d9456ce94a52b04410b
ryan <lrcrews@gmail.com>
2016-11-28 16:17:12 -0800

    sticker stuff, holiday email

---
+++ app/controllers/stickers_controller.rb
@@ -0,0 +1,3 @@
+class StickersController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
 
+  get 'election-stickers' => 'stickers#election_stickers'
+  get 'electoral-math-purchased' => 'stickers#electoral_math_purchased'
+  get 'majority-math-purchased' => 'stickers#majority_math_purchased'
+
   get 'contact' => 'welcome#contact'
   get 'portfolio' => 'welcome#portfolio'
   get 'services' => 'welcome#services'
