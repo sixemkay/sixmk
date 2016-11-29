a386f1efb1c50b4dc313226e497df2bbe7fae2e7
ryan <lrcrews@gmail.com>
2016-11-28 19:54:27 -0800

    separate pages for each sticker

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'election-stickers' => 'stickers#election_stickers'
+  get 'electoral-math' => 'stickers#electoral_math'
   get 'electoral-math-purchased' => 'stickers#electoral_math_purchased'
+  get 'majority-math' => 'stickers#majority_math'
   get 'majority-math-purchased' => 'stickers#majority_math_purchased'
 
   get 'contact' => 'welcome#contact'
